99a9d24bc047023146c94ecc8b7cb319abea56c7
Jungmin Lee <66638168+Jung5921@users.noreply.github.com>
2025-11-17 22:22:47 +0000

    edited script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const API_KEY = "AIzaSyDM3HdtVzup3zlhxrcxnJ9IaQByz3pzibg";
+const SPREADSHEET_ID = "1-5QTUzZbeQDvmTGpdrzmdwiq6w3MhraGWBgI5EVxlJg";
+const SHEET_NAME = "WebDevProject";
+
+
+async function fetchSheetData() {
+    try {
+        const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`;
+
+
+        const response = await fetch(url);
+        const json = await response.json();
+
+
+        return json.values || [];
+    } catch (error) {
+        console.error("Error fetching sheet:", error);
+        return [];
+    }
+}
